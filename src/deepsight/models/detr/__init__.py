2e7bcb149d370b68aca4c852bcf9e384de9415cc
Francesco Gentile <francesco.gentile@studenti.unitn.it>
2024-02-22 17:31:07 +0100

    Exported all classes from detr module

--- src/deepsight/models/detr/__init__.py
+++ src/deepsight/models/detr/__init__.py
@@ -1,13 +1,22 @@
 # Copyright 2024 The DeepSight Team.
 # SPDX-License-Identifier: Apache-2.0
 
-from ._config import Config
+from ._config import Config, Variant
+from ._decoder import Decoder, DecoderLayer
+from ._encoder import Encoder, EncoderLayer
 from ._model import Model
 from ._output import Output
 
 __all__ = [
     # _config
     "Config",
+    "Variant",
+    # _decoder
+    "Decoder",
+    "DecoderLayer",
+    # _encoder
+    "Encoder",
+    "EncoderLayer",
     # _model
     "Model",
     # _output
